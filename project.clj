5de1d20efebf034c0761516f7ab09b945bdbdf87
xinyuwen2022 <792001930@qq.coom>
2022-11-30 11:10:53 -0500

    new

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [quil "2.7.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [quil "3.1.0"]]
   :main snake.core
   :aot :all)
